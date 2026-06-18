01b1784cab28ea515c7a1bb246351b8c8eddc0a9
Sundar-tech11 <sundaravannan366@gmail.com>
2026-06-19 00:21:40 +0530

    Fix server-db import

--- nextstep-ai/server.ts
+++ nextstep-ai/server.ts
@@ -3,7 +3,7 @@ import { GoogleGenAI, Type } from '@google/genai';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import * as db from './server-db.ts';
+import * as db from './server-db';
 
 dotenv.config();
 
